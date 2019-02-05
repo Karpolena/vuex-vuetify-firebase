dcc61d44d17a5d668e51f4fe8b6dd04ff6183458
Karpenko Olena <karp.ol3000@gmail.com>
2019-02-05 16:01:17 +0200

    Orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,33 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list
+                    subheader
+                    two-line
+                >
+                    <v-list-tile
+                        v-for="order of orders"
+                        :key="order.id"
+                    >
+                        <v-list-tile-action>
+                        <v-checkbox 
+                            @change="markDone"
+                            color="success"
+                            :input-value="order.done"
+                        ></v-checkbox>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                        <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                        <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-btn 
+                            class="primary"
+                            :to="'/ad/' + order.adId"
+                        >Open</v-btn>
+                    </v-list-tile>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -12,7 +37,20 @@
 export default {
     data () {
         return {
-            
+            orders: [
+                {
+                    id: "sdf",
+                    name: "Vladilen",
+                    phone: "9-342-234",
+                    adId: "123",
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        markDone() {
+            order.done = true
         }
     }
 }
